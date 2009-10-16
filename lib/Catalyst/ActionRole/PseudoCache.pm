cf7f82c3842fbd61fc534c947b6fd7bd9065c282
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-10-15 23:22:04 -0500

    better pod, cleaner code

--- lib/Catalyst/ActionRole/PseudoCache.pm
+++ lib/Catalyst/ActionRole/PseudoCache.pm
@@ -34,7 +34,18 @@ around BUILDARGS => sub {
    my $class = shift;
    my ($args) = @_;
    if (my $attr = $args->{attributes}) {
-      return $class->$orig( url => $attr->{PCUrl}->[0], %{$args} );
+      my @args = (
+         ($attr->{PCUrl}
+            ? ( url => $attr->{PCUrl}->[0] )
+            : ()
+         ),
+         ($attr->{PCPath}
+            ? ( path => $attr->{PCPath}->[0] )
+            : ()
+         ),
+         %{$args}
+      );
+      return $class->$orig( @args );
    } else {
       return $class->$orig(@_);
    }
@@ -50,14 +61,12 @@ around execute => sub {
 
    if (!$self->is_cached) {
       my $filename = File::Spec->catfile($c->path_to('root'), $self->path);
-      warn $filename;
 
       unlink $filename if stat $filename;
       open my $js_fh, '>', $filename;
 
       $self->$orig(@_);
 
-      warn $c->response->body;
       print {$js_fh} $c->response->body;
       close $js_fh;
 
@@ -73,7 +82,12 @@ around execute => sub {
 
 =head1 SYNOPSIS
 
-sub js :Local {
+package TestApp::Controller::Root;
+
+use Moose;
+BEGIN { extends 'Catalyst::Controller::ActionRole' };
+
+sub js :Local :Does(PseudoCache) PCUrl(/static/js/all.js) {
    my ($self, $c) = @_;
    $c->stash->{js} = [
       'ext3/adapter/ext/ext-base',
@@ -82,8 +96,46 @@ sub js :Local {
          : 'ext3/ext-all',
       @{$c->config->{javascript}{files}}
    ];
-
-   $c->forward( 'js_file' );
 }
+
+sub some_other_action :Local :Does(PseudoCache) PCPath(foo/bar/baz) PCUrl(/static/js/all2.js) {
+   my ($self, $c) = @_;
+   $c->stash->{js} = [
+      'ext3/adapter/ext/ext-base',
+      $c->debug
+         ? 'ext3/ext-all-debug'
+         : 'ext3/ext-all',
+      @{$c->config->{javascript}{files}}
+   ];
+}
+
+=head1 DESCRIPTION
+
+This module was originally made to take the output of
+L<Catalyst::View::JavaScript::Minifier::XS> and store it in a file so that after
+the server has booted once we won't need to generate it again and can let the
+static web server serve up the static file much faster.  Obviously it can be
+used for much more than javascript, but it's mostly made with large, purely
+javascript sites in mind.
+
+=head1 ATTRIBUTES
+
+=head2 PCUrl
+
+Required.
+
+The url that the action will redirect to after it runs once.
+
+=head2 PCPath
+
+Not Required.
+
+When the action gets run the first time it will write it's output to this path.
+
+Defaults to C<$c->path_to('root') . $PCUrl> (roughty)
+
+So using the example given above for the C<js> action, the path will be
+
+ $MyAppLocation/root/static/js/all.js
 
 =end
